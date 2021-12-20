011261da2fefe4fea6b95af31c316b0c3fc08279
gitausam <njorogegitausamuel@gmail.com>
2021-12-20 14:23:00 +0300

    changes enter phone activity layout

--- app/src/main/java/com/zenza/zenzaeats/MainActivity.kt
+++ app/src/main/java/com/zenza/zenzaeats/MainActivity.kt
@@ -2,10 +2,24 @@ package com.zenza.zenzaeats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        val spinner: Spinner = findViewById(R.id.sp_country_codes)
+
+        ArrayAdapter.createFromResource(this,
+            R.array.countries_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
     }
 }
